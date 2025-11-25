50f8699bc5737977933a3ada7848f223377059c6
FedericoPali <palifederico@yahoo.com>
2025-11-25 16:55:31 -0300

    viewRoutes importado en index.js y app.use

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,10 +1,12 @@
 // importamos las rutas de producto
 import productRoutes from "./product.routes.js";
 import salesRoutes from "./sales.routes.js";
+import viewRoutes from "./views.routes.js";
 
 // archivo de barril que contiene y centraliza todas las rutas
 
 export {
     productRoutes,
-    salesRoutes
+    salesRoutes,
+    viewRoutes
 }
